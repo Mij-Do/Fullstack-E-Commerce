a1403c0563e8ea36d6f6773eb5d1fc328db1a683
ahmed <ahmedmijdo2@gmail.com>
2025-09-28 10:06:21 +0300

    20000000000

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -54,7 +54,7 @@ const Login = () => {
         });
         setTimeout(() => {
             location.replace("/");
-        }, 2000);
+        }, 20000000);
     }
     
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
